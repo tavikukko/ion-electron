52f37a41a87566a80065a3ac81dd5cdd21de3af8
Tomi Tavela <tomi@Tomis-MBP.Home>
2018-04-04 17:14:16 +0300

    setting up the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,6 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { Electron } from '../providers/electron'
 
 
@@ -29,8 +26,6 @@ import { Electron } from '../providers/electron'
     ListPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
     Electron,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
