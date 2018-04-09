4968b5a1dee7f35e5f5d61271f7c35dc7783d98a
Tomi Tavela <tomi@Tomis-MacBook-Pro.local>
2018-04-09 15:24:42 +0300

    refactoring

--- src/providers/powershell/powershell.ts
+++ src/providers/powershell/powershell.ts
@@ -22,8 +22,6 @@ export class Powershell {
   }
 
   invokePowerShellScript(script, params) {
-    window.URL = window.URL;
-
     return new Promise((resolve, reject) => {
       this.ps.addCommand(`./www/assets/ps-scripts/${script}`, params);
       this.ps.invoke()
@@ -34,7 +32,6 @@ export class Powershell {
           this.ps.dispose();
           reject(err);
         });
-
     });
   }
 
